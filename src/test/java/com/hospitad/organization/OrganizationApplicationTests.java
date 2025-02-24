a55485322bdcbf383edbfc50c6802d6c7295b6c3
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2025-02-24 16:00:43 +0100

    Update OrganizationApplicationTests.java

--- src/test/java/com/hospitad/organization/OrganizationApplicationTests.java
+++ src/test/java/com/hospitad/organization/OrganizationApplicationTests.java
@@ -3,6 +3,8 @@ package com.hospitad.organization;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// some modfiticatipns
+
 @SpringBootTest
 class OrganizationApplicationTests {
 
